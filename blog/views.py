6f4b6793c3fb4e5dca4128221ff9a48572d08976
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-15 01:53:26 +0900

    Django 폼

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+
 
 
 def post_list(request):
@@ -15,3 +18,34 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {
         'post': post
     })
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+    else:
+        form = PostForm()
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+
+    return render(request, 'blog/post_edit.html', {
+        'form': form
+    })
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
